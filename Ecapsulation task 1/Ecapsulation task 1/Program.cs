106a631ff41959e1d1675ba7a87f4b8928c31bb4
Iain Foster <9925082@student.toiohomai.ac.nz>
2019-03-06 16:37:47 +1300

    task 1 & 2

---
+++ Ecapsulation task 1/Ecapsulation task 1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecapsulation_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Creditcard c1 = new Creditcard();
+            Console.WriteLine("This program is to store credit card info and return public information back");
+            Console.WriteLine("Please enter your name: ");
+            c1.Name = Console.ReadLine();
+            Console.WriteLine("Please enter the Creditcards number: ");
+            c1.Cardnumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the cards security number: ");
+            c1.Pin = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the expiry date (DD/../....):  ");
+            c1.Expire.Day = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the expiry date (../MM/....) :");
+            c1.Expire.Month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the expiry date (../../YYYY) :");
+            c1.Expire.Year = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("................Your public information.................. ");
+            Console.WriteLine($"Creditcard holders name: {c1.Name}");
+            Console.WriteLine($"Expiration Date: {c1.Expire.Day}/{c1.Expire.Month}/{c1.Expire.Year}");
+            Console.ReadKey();
+        }
+    }
+    
+    class Creditcard
+    {
+        string name;
+        int cardnumber;
+        int pin;
+        ExpiryDate expire = new ExpiryDate();
+
+        public string Name { get { return name; } set{ name = value; } }
+        public int Cardnumber { private get { return cardnumber; } set { cardnumber = value; } }
+        public int Pin { private get { return pin; } set { pin = value; } }
+        public ExpiryDate Expire { get { return expire; } set { expire = value; } }
+
+        public Creditcard(string _name, int _cardnumber, int _pin, ExpiryDate _expire)
+        {
+            Name = _name;
+            Cardnumber = _cardnumber;
+            Pin = _pin;
+            Expire = _expire;
+        }
+
+        public Creditcard()
+        {
+
+        }
+    }
+
+    class ExpiryDate
+    {
+        int day;
+        int month;
+        int year;
+
+        public int Day {  get { return day; } set { day = value; } }
+        public int Month { get { return month; } set { month = value; } }
+        public int Year { get { return year; } set { year = value; } }
+
+    }
+    
+}
---
+++ encapsulation task 2/encapsulation task 2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encapsulation_task_2
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int size = 5;//Change this to change the max size of the collection;
+            string check = "";
+            int counter = 0;
+            List<Person> people = new List<Person>();
+            do
+            {
+
+                Console.Write("Would you like to create a new person record (y/n): ");
+                check = Console.ReadLine().ToLower();//Reads in user input and converts to lowercase
+                if (check == "y")//If the user entered a y or Y
+                {
+                    Console.Clear();
+                    Console.WriteLine("Enter The person's details...");
+                    Console.Write("First name: ");
+                    string fname = Console.ReadLine();
+                    Console.Write("{0}'s last name: ", fname);
+                    string lname = Console.ReadLine();
+                    Console.Write("{0} {1}'s age: ", fname, lname);
+                    int age = int.Parse(Console.ReadLine());
+                    people.Add(new Person(fname, lname, age));//Add a new person to the list of people.
+                    counter = counter + 1;
+                }
+                else if (check != "y" && check != "n")
+                {
+                    Console.WriteLine("That was not a valid input, please try again.");
+                }
+            } while (counter < size && check != "n");
+
+            Console.Clear();
+            Console.WriteLine("******* {0} Sets of Details *******", Person.counter);
+            foreach (Person x in people)
+            {
+                Console.WriteLine("Name: {0} {1}\nAge: {2}\n", x.Fname, x.Lname, x.Age);
+            }
+        }
+    }
+
+    class Person
+    {
+        /*Private Attributes*/
+        private string fname;
+        private string lname;
+        private int age;
+        /*Static Attribute - note that it's accessible to the class not the object*/
+        public static int counter = 0;
+
+        /*Public Getters and Setters*/
+        public string Fname { get { return fname; } set { fname = value; } }
+        public string Lname { get { return lname; } set { lname = value; } }
+        public int Age { get { return age; } set { age = value; } }
+
+        /*Constructor*/
+        public Person(string _fname, string _lname, int _age)
+        {
+            Fname = _fname;
+            Lname = _lname;
+            Age = _age;
+            Console.WriteLine("New Person Object Created");
+            counter++;
+        }
+    }
+}
